accf3f0600e40af3a1d67d9b9c63a0b1f0c9305a
Lucas Brasil Silva <126355710+Lucas-Brasil-Silva@users.noreply.github.com>
2025-03-02 23:42:03 +0000

    Desafio-Novato

--- xadrez.c
+++ xadrez.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
+// Este programa simula os movimentos de 3 peças de xadrez: Torre, Bispo e Rainha. Além de imprime na tela os comandos de movimento de cada uma delas.
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    
+    // Declaração das constantes com o número de movimentos das peças.
+    const int torreMovimento = 5;
+    const int bispoMovimento = 5;
+    const int rainhaMovimento = 8;
+    
+    // Declaração das variáveis de iteraçào dos loops.
+    int j = 0;
+    int i = 0;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // Move a Torre 5 casas para a Direita.
+    do {
+        printf("Direita!\n"); //Imprime os movimentos da Torre.
+        i++;
+    } while (i < torreMovimento);
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // Move o Bispo 5 casas para Cima e Direita.
+    while (j < bispoMovimento)
+    {
+        printf("Cima, Direita!\n"); //Imprime os movimentos do Bispo.
+        j++;
+    }
+    
+    // Move a rainha 8 casas para a esquerda.
+   for (int h = 0; h < rainhaMovimento; h++)
+   {
+    printf("Esquerda!\n"); //Imprime os movimentos da Rainha.
+   }
+    
+    return 0;
+}
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
@@ -27,6 +44,3 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
-}
